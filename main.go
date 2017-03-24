ea59c480088f6bb0cb614780035ba0425a45f57a
Yak Jun Xiang <snowypowers@gmail.com>
2017-03-24 12:57:34 +0800

    Add Store and API link to data gov

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/joho/godotenv"
 	"gopkg.in/telegram-bot-api.v4"
+
+	"github.com/snowypowers/sgweatherbot/store"
 )
 
 func main() {
@@ -17,14 +19,23 @@ func main() {
 
 	botToken := os.Getenv("BOT_TOKEN")
 	webLink := os.Getenv("WEBSITE_LINK")
+	apiKey := os.Getenv("DATA_API_KEY")
 
+	//Store
+	s := store.Store()
+	wf2 := store.Subscription{
+		"wf2",
+		"https://api.data.gov.sg/v1/environment/2-hour-weather-forecast",
+		apiKey,
+		300}
+	wf2Close := s.Listen(wf2)
+
+	//Bot
 	bot, err := tgbotapi.NewBotAPI(botToken)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	bot.Debug = true
-
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	_, err = bot.SetWebhook(tgbotapi.NewWebhookWithCert(webLink+bot.Token, "cert.pem"))
@@ -39,6 +50,27 @@ func main() {
 	go http.ListenAndServeTLS("0.0.0.0:8443", "cert.pem", "key.pem", nil)
 
 	for update := range updates {
-		log.Printf("%+v\n", update)
+		//Command Parsing
+		cmd := update.Message.Command()
+		args := update.Message.CommandArguments()
+		out := ""
+		switch cmd {
+		case "/wf2":
+			out = getWF2(s.Value(wf2), args)
+		case "/start":
+			out = "Hello! Welcome! This bot is under construction!"
+		default:
+			out = "hi"
+		}
+		log.Printf("%+v\n", update.Message)
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, out)
+		bot.Send(msg)
 	}
+
+	//Teardown
+	close(wf2Close)
+}
+
+func getWF2(data string, args string) string {
+	return "haha"
 }
---
+++ store/store.go
@@ -0,0 +1,89 @@
+package store
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"sync"
+	"time"
+)
+
+type Subscription struct {
+	Name     string
+	Endpoint string
+	Key      string
+	Interval int
+}
+
+type store struct {
+	Client *http.Client
+	Subs   []Subscription
+}
+
+var instance *store
+var once sync.Once
+var subs []Subscription
+var db map[string]string
+
+//Store Singleton instance method.
+//Returns a reference to the store object
+func Store() *store {
+	once.Do(func() {
+		client := &http.Client{
+			Timeout: time.Second * 10,
+		}
+		subs := make([]Subscription, 1)
+		instance = &store{client, subs}
+		db = make(map[string]string)
+	})
+	return instance
+}
+
+//Listens to a API endpoint, calling it at interval, returning JSON body as string through out.
+//Returns a
+func (a store) Listen(sub Subscription) chan string {
+	subs = append(subs, sub)
+	quit := make(chan string)
+	ticker := time.NewTicker(time.Second * time.Duration(sub.Interval))
+	//Initial retrieval
+	res := retrieve(sub)
+	db[sub.Name] = res
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				res := retrieve(sub)
+				db[sub.Name] = res
+			case <-quit:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+
+	return quit
+}
+
+func (a store) Value(sub Subscription) string {
+	return db[sub.Name]
+}
+
+//Retrieve immediately sends an API request and attempts to retrieve the information available.
+//Returns the JSON body in string format.
+func retrieve(sub Subscription) string {
+	req, err := http.NewRequest("GET", sub.Endpoint, nil)
+	req.Header.Add("api-key", sub.Key)
+	res, err := instance.Client.Do(req)
+
+	if err != nil {
+		log.Fatal(err)
+		return ""
+	} else {
+		defer res.Body.Close()
+		bs, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return string(bs)
+	}
+}
